08b6bbaa4ea9e35dec087fe4b47b3a6cf8a6850d
Anjana Paul <anjanapaul0614@gmail.com>
2022-12-22 11:56:44 +0400

    cvss calculator with severity

---
+++ cvss_severity.py
@@ -0,0 +1,70 @@
+
+import math
+
+impact = input("enter the value for impact ")
+print(impact)
+exploitability = input("enter the value exploitability ")
+print(exploitability)
+temporalscore = float(input("enter the value for temporalscore "))
+print(temporalscore)
+environmentalscore = float(input(" enter the value for environmentalscore "))
+print(environmentalscore)
+
+
+score_mapping = {
+  "remote": 10.0,
+  "local": 7.5,
+  "physical": 5.0
+}
+
+
+def cvss_score(impact, exploitability):
+  return (0.6 * impact) + (0.4 * exploitability) - 1.5
+
+
+def cvss_temporal_score(base_score, temporalscore):
+  return base_score * temporalscore
+
+
+def cvss_environmental_score(tempscore, environmentalscore):
+  return tempscore * environmentalscore
+
+
+def round_to_tenth(score):
+  return round(score * 10) / 10
+
+
+def calculate_cvss_score(impact, exploitability, temporalscore, environmentalscore):
+
+  impact = score_mapping[impact]
+  exploitability = score_mapping[exploitability]
+
+  
+  base_score = cvss_score(impact, exploitability)
+
+  
+  tempscore = cvss_temporal_score(base_score, temporalscore)
+
+ 
+  environmental_score = cvss_environmental_score(tempscore, environmentalscore)
+
+  score = round_to_tenth(environmental_score)
+
+# severity level
+  if score >= 9.0:
+    severity = "Critical"
+  elif score >= 7.0:
+    severity = "High"
+  elif score >= 5.0:
+    severity = "Medium"
+  else:
+    severity = "Low"
+
+  
+  return score, severity
+
+
+
+
+print(calculate_cvss_score(impact, exploitability, temporalscore, environmentalscore))
+
