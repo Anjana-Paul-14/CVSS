f95ca3b49044d453beb0f4a030bd94d61b0d8f43
Anjana Paul <anjanapaul0614@gmail.com>
2023-02-13 16:46:16 +0530

    calculator updated

--- app.py
+++ app.py
@@ -20,26 +20,26 @@ value_mapping = {
 def calculate_cvss_score(base_score):
  
   if base_score >= 9.0:
-    severity = "Critical"
+    severity = "critical"
   elif base_score >= 7.0:
-    severity = "High"
+    severity = "high"
   elif base_score >= 4.0:
-    severity = "Medium"
+    severity = "medium"
   else:
-    severity = "Low"
+    severity = "low"
   return base_score, severity
 
 @app.route('/',methods=['POST','GET'])
 
-def index ():
+def cvss():
    # return ("hello")
    if request.method=='POST': 
-      access_vector = request.form["access_vector"]
-      access_complexity = request.form["access_complexity"]
-      authentication = request.form["authentication"]
-      confidentiality_impact = request.form["confidentiality_impact"]
-      integrity_impact = request.form["integrity_impact"]
-      availability_impact = request.form["availability_impact"]
+      access_vector = float(request.form["access_vector"])
+      access_complexity = float(request.form["access_complexity"])
+      authentication = float(request.form["authentication"])
+      confidentiality_impact = float(request.form["confidentiality_impact"])
+      integrity_impact = float(request.form["integrity_impact"])
+      availability_impact = float(request.form["availability_impact"])
 
       access_vector_value = value_mapping[access_vector]
       access_complexity_value = value_mapping[access_complexity]
@@ -53,8 +53,8 @@ def index ():
 
       cvss_score, severity = calculate_cvss_score(base_score)
 
-      return render_template("result.html", cvss_score=cvss_score, severity=severity)
-   return render_template("cvss_1.html")
+      return render_template("result.html", access_vector=access_vector, access_complexity=access_complexity, authentication=authentication, confidentiality_impact=confidentiality_impact,integrity_impact=integrity_impact,availability_impact=availability_impact, cvss_score=cvss_score, severity=severity)
+   return render_template("cvss.html")
 
   
 
